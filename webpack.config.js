ede18ea19d648cfdbe01ce78cc310cef93c6d446
HebeHan <doveskila@gmail.com>
2018-08-12 23:42:13 +0800

    first step

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,23 @@
 module.exports = {
-    entry:"./index.jsx",
-    output: {
-
+    mode:'development',
+    entry:'./src/app/index.jsx',
+    output:{
+        filename:'bundle.js',
+        path:__dirname
+    },
+    module:{
+        rules: [
+            {
+                test:/\.css$/,
+                use:['style-loader','css-loader']
+            },
+            {
+                test:/\.js[x]?$/,
+                exclude:/node_modules/,
+                use:{
+                    loader:'babel-loader',
+                }
+            }
+        ]
     }
-
-
 }
