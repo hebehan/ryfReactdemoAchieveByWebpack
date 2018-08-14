19b5ddbb289707b85e9b213c8e73e870e476f645
HebeHan <doveskila@gmail.com>
2018-08-14 21:19:18 +0800

    update

---
+++ src/app/components/Demo2.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+class Demo2 extends Component {
+    render() {
+        return (
+            <div>
+                <h1>from demo2</h1>
+            </div>
+        );
+    }
+};
+
+export default Demo2;
--- src/app/index.js
+++ src/app/index.js
@@ -1,77 +1,60 @@
-import React,{Component} from 'react';
+import React, {Component} from 'react';
 import {render} from 'react-dom';
-import {BrowserRouter,Switch,Route,Link} from 'react-router-dom';
+import {BrowserRouter, Switch, Route, Link, Router} from 'react-router-dom';
 import List from "./components/List";
+import Demo1 from './components/Demo1'
+import Demo2 from './components/Demo2'
+import {DatePicker} from 'antd';
 
 var style = require('./style/demo.css')
 
 var items = [
     {
-        className:"item",
-        path:"/demo1",
-        name:"demo1",
-        comp:"Demo1"
-    },
-    {
-        path:"/demo2",
-        name:"demo2",
-        comp:"Demo2"
+        className: "item",
+        path: "/demo1",
+        name: "demo1",
+        comp: "Demo1"
+    }, {
+        path: "/demo2",
+        name: "demo2",
+        comp: "Demo2"
     }
 ];
 class App extends Component {
     render() {
         let itembean = [];
         for (let i = 1; i < 30; i++) {
-            itembean.push(<li className="item"><Link to={"/demo"+i}>{"Demo"+i}</Link></li>)
+            itembean.push(
+                <li className="item">
+                    <Link to={"/demo" + i}>{"Demo" + i}</Link>
+                </li>
+            )
         }
-        // let itembean = items.map(function (item, index) {
-        //     return <li className={}><Link to="/demo1">Demo1</Link></li>;
-        // })
+        // let itembean = items.map(function (item, index) {     return <li
+        // className={}><Link to="/demo1">Demo1</Link></li>; })
         return (
-            <div>
-                <header>
-                    <ul>
+            <BrowserRouter >
+                <div >
+
+                    <ul className="twoline">
                         {/*<li className="item"><Link to="/demo1">Demo1</Link></li>*/}
                         {/*<li className="item"><Link to="/demo2">Demo2</Link></li>*/}
                         {itembean}
                     </ul>
-                </header>
-                <main className="main">
-                    <Switch>
+
+                    <Switch >
+
                         <Route exact path='/' component={Demo1}/>
                         <Route path='/demo1' component={Demo1}/>
                         <Route path='/demo2' component={Demo2}/>
                         <Route path='*' component={Demo1}/>
                     </Switch>
-                </main>
-            </div>
+                </div>
+            </BrowserRouter>
+
         );
     }
 };
 
-class Demo1 extends Component {
-    render() {
-        return (
-            <div>
-                <h1>from demo1</h1>
-            </div>
-        );
-    }
-};
-class Demo2 extends Component {
-    render() {
-        return (
-            <div>
-                <h1>from demo2</h1>
-            </div>
-        );
-    }
-};
-
-render((
-        <BrowserRouter >
-            <Route path="/" component={App}/>
-        </BrowserRouter>
-    )
-,document.querySelector('#app'))
-
+render(
+    <App/>, document.querySelector('#app'))
