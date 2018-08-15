3495f016a9149244f730f1d8ad6a865cc6f3a167
HebeHan <doveskila@gmail.com>
2018-08-15 17:23:11 +0800

    antd

---
+++ src/app/components/Demo3.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+class Demo3 extends Component {
+  render() {
+    return (
+      <div>
+        <p>from demo3</p>
+      </div>
+    )
+  }
+}
+export default Demo3;
--- src/app/index.js
+++ src/app/index.js
@@ -5,34 +5,57 @@ import List from "./components/List";
 import Demo1 from './components/Demo1'
 import Demo2 from './components/Demo2'
 import {DatePicker} from 'antd';
-
+import Demo3 from './components/Demo3';
+import { Layout } from 'antd';
+const { Header, Footer, Sider, Content } = Layout;
 var style = require('./style/demo.css')
 
 var items = [
     {
         className: "item",
         path: "/demo1",
-        name: "demo1",
-        comp: "Demo1"
+        comp: Demo1
     }, {
+        className: "item",
         path: "/demo2",
-        name: "demo2",
-        comp: "Demo2"
+        comp: Demo2
+    },{
+        className: "item",
+        path: "/demo3",
+        comp: Demo3
     }
 ];
 class App extends Component {
     render() {
         let itembean = [];
-        for (let i = 1; i < 30; i++) {
+        let routebean = [];
+        routebean.push(
+            <Route exact path='/' component={items[0].comp}/>
+        )
+        // routebean.push(
+        //     <Route path='*' component={items[0].comp}/>
+        // )
+        for (let i = 0; i < items.length; i++) {
             itembean.push(
-                <li className="item">
-                    <Link to={"/demo" + i}>{"Demo" + i}</Link>
+                <li className={items[i].className}>
+                    <Link to={items[i].path}>{items[i].comp.name}</Link>
                 </li>
             )
+
+            routebean.push(
+                // "<Route path='"+items[i].path+ "' component={"+items[i].comp+"}/>"
+                <Route path={items[i].path} component={items[i].comp}/>
+            )
         }
+        // console.log(routebean)
         // let itembean = items.map(function (item, index) {     return <li
         // className={}><Link to="/demo1">Demo1</Link></li>; })
         return (
+
+            
+
+            <Layout>
+            <Sider >
             <BrowserRouter >
                 <div >
 
@@ -44,14 +67,23 @@ class App extends Component {
 
                     <Switch >
 
-                        <Route exact path='/' component={Demo1}/>
+                        {routebean}
+
+                        {/* <Route exact path='/' component={Demo1}/>
                         <Route path='/demo1' component={Demo1}/>
                         <Route path='/demo2' component={Demo2}/>
-                        <Route path='*' component={Demo1}/>
+                        <Route path='*' component={Demo1}/> */}
+
                     </Switch>
                 </div>
             </BrowserRouter>
-
+            </Sider>
+            <Layout>
+              <Header>Header</Header>
+              <Content>Content</Content>
+              <Footer>Footer</Footer>
+            </Layout>
+          </Layout>
         );
     }
 };
